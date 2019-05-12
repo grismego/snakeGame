d3dc90c535a964cf8d1ca84af1e5145e8122473a
Алексей Кунчий <gris.mego@gmail.com>
2019-05-12 13:31:42 +0700

    gh-pages

--- src/scripts/lib/command.js
+++
@@ -1,6 +0,0 @@
-export const KeyCode = {
-  LEFT: 37,
-  UP: 38,
-  RIGHT: 39,
-  DOWN: 40
-};
--- src/scripts/lib/random.js
+++
@@ -1,3 +0,0 @@
-export const generateRandomNumber = (min, max) => (
-  Math.floor(min + Math.random() * (max - min + 1))
-);
